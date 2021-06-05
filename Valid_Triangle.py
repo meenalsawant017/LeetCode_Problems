7f0c62c1a6d3bfb517e37f3ccffbb0c43670e206
Meenal Sawant <57767777+meenalsawant017@users.noreply.github.com>
2021-06-05 12:19:52 -0400

    Update Valid_Triangle.py

--- Valid_Triangle.py
+++ Valid_Triangle.py
@@ -9,10 +9,8 @@ def validTriangle(A):
         a,b,c = A[i], A[i+1], A[i+2]
 
         if a+b>c and b+c>a and a+c>b:
-            #print('appending 1 in B: a,b,c', a,b,c)
             B.append(1)
         else:
-            #print('appending 0 in B: a,b,c', a,b,c)  
             B.append(0)
         i += 1
     return B
