c80116ff54ca0ed3005e2a63e9f24d039e81120f
Meenal Sawant <57767777+meenalsawant017@users.noreply.github.com>
2021-10-03 10:46:25 -0400

    Update countWaysToSplit.py

--- CodeSignal/countWaysToSplit.py
+++ CodeSignal/countWaysToSplit.py
@@ -1,20 +1,36 @@
 def countWaysToSplit(s):
-   listS= list(s)
-   # print (listS)
-   lenS = len(listS)
+   lenS = len(s)
    count = 0
 
-   for i in range(1,lenS):
+   for i in range(1,lenS-1):
       for j in range(i+1, lenS):
-            ab = s[0:j]
-            bc = s[i:lenS]
-            ca = s[j:lenS]
-            print("AB is :",ab,"BC is:",bc,"CA is:",ca)
+            ab = s[0:i]
+            bc = s[i:j]
+            ca = s[j:]
             if ab != bc and bc != ca  and ca != ab :
                 count += 1 
-                print(count)  
    return count
 
 s = 'xzxzx'
 res = countWaysToSplit(s)
 print(res)
+ 
+s = 'xzxzxzxzxz' #30
+print("")
+print('string: ', s)
+print(countWaysToSplit(s))
+
+s = 'xxxxxxxxxx' #24
+print("")
+print('string: ', s)
+print(countWaysToSplit(s))
+
+s = 'gggggggggggggggggggggggggggggg' # 366
+print("")
+print('string: ', s)
+print(countWaysToSplit(s))
+
+s = 'gfgfgfgfgfgfgfgfgfgfgfgfgfgfgf' #387
+print("")
+print('string: ', s)
+print(countWaysToSplit(s))
