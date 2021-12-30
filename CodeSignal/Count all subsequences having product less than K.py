0ab6a915646d4d2901f8cd0bb4cc4ab598de8769
Meenal Sawant <57767777+meenalsawant017@users.noreply.github.com>
2021-12-30 15:05:29 -0500

    Add files via upload

---
+++ CodeSignal/Count all subsequences having product less than K.py
@@ -0,0 +1,29 @@
+def productSubSeqCount(arr, k):
+    n = len(arr)
+    dp = [[0 for i in range(n + 1)]
+             for j in range(k + 1)]
+    for i in range(1, k + 1):
+        for j in range(1, n + 1):
+             
+            # number of subsequence
+            # using j-1 terms
+            dp[i][j] = dp[i][j - 1]
+             
+            # if arr[j-1] > i it will
+            # surely make product greater
+            # thus it won't contribute then
+            if arr[j - 1] <= i and arr[j - 1] > 0:
+                 
+                # number of subsequence
+                # using 1 to j-1 terms
+                # and j-th term
+                dp[i][j] += dp[i // arr[j - 1]][j - 1] + 1
+    return dp[k][n]
+ 
+# Driver code
+A = [1,2,3,4]
+k = 10
+print(productSubSeqCount(A, k))
+ 
+# This code is contributed
+# by pk_tautolo
