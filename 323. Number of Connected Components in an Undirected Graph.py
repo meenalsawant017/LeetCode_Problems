cefcc5d8b33a4a5a7effb2d8750bd94da5ad8485
Meenal Sawant <57767777+meenalsawant017@users.noreply.github.com>
2022-02-01 13:52:56 -0500

    Add files via upload

---
+++ 323. Number of Connected Components in an Undirected Graph.py
@@ -0,0 +1,36 @@
+#similiar to Leetcode 547
+def countComponent(n, edges):
+      par = [i for i in range(n)]
+      rank = [1] * n
+
+      def find(n1):
+            res = n1
+
+            while res != par[res]:
+                  res = par[res]
+            return res
+
+      def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+
+            if p1 == p2:
+                  return 0
+
+            if rank[p1] < rank[p2]:
+                  par[p1] = p2 
+                  rank[p2] += rank[p1]
+            else:
+                  par[p2] = p1
+                  rank[p1] += rank[p2]
+            return 1
+
+      res = n
+
+      for n1, n2 in edges:
+            res -= union(n1, n2)
+      return res
+
+            
+n = 5
+edges = [[0,1],[1,2], [3,4]]
+print(countComponent(n, edges))
